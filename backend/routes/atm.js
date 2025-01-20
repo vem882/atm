eda2b9b6bda84eef7697974f96205b62d2f5f201
vem882 <vem882@gmail.com>
2025-01-20 18:03:01 +0000

    Automaattinen päivitys: Mon Jan 20 18:03:01 UTC 2025

--- backend/routes/atm.js
+++ backend/routes/atm.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const atmController = require('../controllers/atm/atm').default;
+const atmController = require('../controllers/atm/atm');
 
 // Reitti ATM:n sarjanumeron tarkistamiseen
 router.get('/atm/:serialNumber', atmController.checkATMSerialNumber);
