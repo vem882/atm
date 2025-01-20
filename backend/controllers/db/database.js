96ff167acae39c640c8cc060b8efd4b6afa2f331
vem882 <vem882@gmail.com>
2025-01-20 17:42:23 +0000

    Automaattinen päivitys: Mon Jan 20 17:42:23 UTC 2025

--- backend/controllers/db/database.js
+++ backend/controllers/db/database.js
@@ -4,12 +4,11 @@ const dotenv=require('dotenv');
 dotenv.config();
 
 //const myConnectionString = "mysql://netuser:netpass@localhost:3306/netdb";
-const myConnectionString = process.env.MYSQL_SERVER;
-if (!myConnectionString) {
+const connection=mysql.createPool(process.env.MYSQL_SERVER);
+if (!connection) {
     throw new Error('MYSQL_SERVER environment variable is not defined');
   }
   
-  console.log('Using MySQL connection string:', myConnectionString);
+  console.log('Using MySQL connection string:', connection);
   
-  const connection = mysql.createPool(myConnectionString);
   module.exports = connection;
