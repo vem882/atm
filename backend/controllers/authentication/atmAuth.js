e8a0ca9935dcf8e7b496a760ef94ea4fbc0d4ed0
vem882 <vem882@gmail.com>
2025-01-21 16:24:31 +0000

    Automaattinen päivitys: Tue Jan 21 16:24:31 UTC 2025

--- backend/controllers/authentication/atmAuth.js
+++ backend/controllers/authentication/atmAuth.js
@@ -3,18 +3,21 @@ const cardModel = require('../../models/card_model');
 const atmModel = require('../../models/atm');
 
 const login = async (req, res) => {
+console.log('Login request received:', req.params, req.body);
   const { serialNumber } = req.params;
   const { cardNumber, pin } = req.body;
 
   try {
     // Tarkista, onko ATM olemassa
     const atm = await atmModel.getATMBySerialNumber(serialNumber);
+    console.log('ATM:', atm); // Debug-tuloste
     if (!atm) {
       return res.status(404).json({ message: 'ATM not found' });
     }
 
     // Hae kortin tiedot
     const card = await cardModel.getCardByNumberAndPin(cardNumber, pin);
+    console.log('Card:', card); // Debug-tuloste
     if (!card) {
       return res.status(401).json({ message: 'Invalid card number or PIN' });
     }
