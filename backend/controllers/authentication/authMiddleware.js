a8fe6251ec891cb0c8a6a79f08324f8edf69cc96
vem882 <vem882@gmail.com>
2025-02-11 17:39:00 +0000

    Automaattinen päivitys: Tue Feb 11 17:39:00 UTC 2025

--- backend/controllers/authentication/authMiddleware.js
+++ backend/controllers/authentication/authMiddleware.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
+const atmModel = require('./models/atmModel'); // Olettaen, että sinulla on ATM-malli
 
-const authMiddleware = (req, res, next) => {
+const authMiddleware = async (req, res, next) => {
   try {
     // Hae token Authorization-headerista
     const token = req.headers.authorization?.split(' ')[1]; // Bearer <token>
@@ -13,6 +14,22 @@ const authMiddleware = (req, res, next) => {
 
     // Lisää dekoodatut tiedot request-olioon
     req.user = decoded;
+
+    // Hae ATM:n sarjanumero pyynnöstä 
+    const { serialNumber } = req.body; 
+    if (!serialNumber) {
+      return res.status(400).json({ message: 'Serial number missing' });
+    }
+
+    // Tarkista, onko ATM olemassa
+    const atm = await atmModel.getATMBySerialNumber(serialNumber);
+    console.log('ATM:', atm); // Debug-tuloste
+    if (!atm) {
+      return res.status(404).json({ message: 'ATM not found' });
+    }
+
+    // Lisää ATM tiedot request-olioon
+    req.atm = atm;
 
     // Jatka pyynnön käsittelyä
     next();
