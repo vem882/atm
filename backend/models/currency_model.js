8cbff163617834c0b6acc73891987770b9a3bd74
vem882 <vem882@gmail.com>
2025-02-15 15:58:45 +0000

    Automaattinen päivitys: Sat Feb 15 15:58:45 UTC 2025

--- backend/models/currency_model.js
+++ backend/models/currency_model.js
@@ -1,19 +1,20 @@
 const connection = require('../controllers/db/database'); // Adjust the path as needed
 
-const updateCurrencyRates = async (rates, forceUpdate) => {
+const updateCurrencyRates = async (rates, symbols, forceUpdate) => {
   const querySelect = 'SELECT * FROM currency WHERE currency_type = ?';
-  const queryInsert = 'INSERT INTO currency (currency_type, currency_value) VALUES (?, ?)';
-  const queryUpdate = 'UPDATE currency SET currency_value = ? WHERE currency_type = ?';
+  const queryInsert = 'INSERT INTO currency (currency_type, currency_value, currency_symbol) VALUES (?, ?, ?)';
+  const queryUpdate = 'UPDATE currency SET currency_value = ?, currency_symbol = ? WHERE currency_type = ?';
 
   return new Promise(async (resolve, reject) => {
     try {
       for (const [currencyCode, rate] of Object.entries(rates)) {
+        const symbol = symbols[currencyCode] || currencyCode; // Use currency code as default symbol if not provided
         const [rows] = await connection.promise().query(querySelect, [currencyCode]);
 
         if (rows.length === 0) {
-          await connection.promise().query(queryInsert, [currencyCode, rate]);
+          await connection.promise().query(queryInsert, [currencyCode, rate, symbol]);
         } else if (forceUpdate) {
-          await connection.promise().query(queryUpdate, [rate, currencyCode]);
+          await connection.promise().query(queryUpdate, [rate, symbol, currencyCode]);
         }
       }
       resolve();
